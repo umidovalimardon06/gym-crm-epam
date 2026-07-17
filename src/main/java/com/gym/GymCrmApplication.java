a092a322c6526c8700537de022dd5b3d36861a94
aumidov <umidovalimardon06@gmail.com>
2026-07-17 20:24:16 +0500

    add spring boot entry point

---
+++ src/main/java/com/gym/GymCrmApplication.java
@@ -0,0 +1,9 @@
+package com.gym;
+
+import org.springframework.boot.SpringApplication;
+
+public class GymCrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmApplication.class, args);
+    }
+}
