5cdaa4947b373c562165ca9b96e99364fde58f3b
Umidov Alimardon <umidovalimardon06@gmail.com>
2026-08-03 16:47:34 +0500

    Add PostMapping and PutMapping annotations in AuthController

--- src/main/java/com/gym/infrastructure/web/controller/AuthController.java
+++ src/main/java/com/gym/infrastructure/web/controller/AuthController.java
@@ -23,7 +23,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
