7ce4a1723798fe03babe778497c740738d9c9c43
aumidov <umidovalimardon06@gmail.com>
2026-08-11 22:45:07 +0500

    update feign include id && logging properties

--- gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadFeignConfig.java
+++ gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadFeignConfig.java
@@ -2,6 +2,7 @@ package com.gym.infrastructure.workload;
 
 import com.gym.infrastructure.secuirty.JwtService;
 import feign.RequestInterceptor;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,11 @@ public class WorkloadFeignConfig {
         return requestTemplate -> {
             String token = jwtService.generateServiceToken("gym-crm-service");
             requestTemplate.header("Authorization", "Bearer " + token);
+
+            String transactionId = MDC.get("transactionId");
+            if (transactionId != null) {
+                requestTemplate.header("X-Transaction-Id", transactionId);
+            }
         };
     }
 }
