e957c09b0c14eae693b138665b2764f38e876961
aumidov <umidovalimardon06@gmail.com>
2026-08-11 23:10:00 +0500

    refactor typo

--- gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadFeignConfig.java
+++ gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadFeignConfig.java
@@ -2,12 +2,15 @@ package com.gym.infrastructure.workload;
 
 import com.gym.infrastructure.secuirty.JwtService;
 import feign.RequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WorkloadFeignConfig {
+    private final Logger log = LoggerFactory.getLogger(WorkloadFeignConfig.class);
 
     @Bean
     public RequestInterceptor workloadAuthInterceptor(JwtService jwtService) {
