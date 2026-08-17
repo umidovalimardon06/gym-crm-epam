f5c84bca763d1b94bc8af6af1c37eae8313d7063
aumidov <umidovalimardon06@gmail.com>
2026-08-17 16:23:07 +0500

    add new type workload request type to converter

--- gym-service/src/main/java/com/gym/infrastructure/config/JmsConfig.java
+++ gym-service/src/main/java/com/gym/infrastructure/config/JmsConfig.java
@@ -1,6 +1,8 @@
 package com.gym.infrastructure.config;
 
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
+import com.gym.infrastructure.workload.WorkloadRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MessageType;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -13,6 +15,7 @@ public class JmsConfig {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setTypeIdMappings(Map.of("workloadRequest", WorkloadRequest.class));
         return converter;
     }
 }
--- workload-service/src/main/java/com/epam/workload/config/JmsConfig.java
+++ workload-service/src/main/java/com/epam/workload/config/JmsConfig.java
@@ -1,5 +1,7 @@
 package com.epam.workload.config;
 
+import java.util.Map;
+import com.epam.workload.dto.WorkloadMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MessageType;
@@ -13,6 +15,7 @@ public class JmsConfig {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setTypeIdMappings(Map.of("workloadRequest", WorkloadMessage.class));
         return converter;
     }
 }
