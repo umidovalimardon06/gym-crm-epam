29c6ab9430776bbb1c0bf7e463fd40bb740f66de
aumidov <umidovalimardon06@gmail.com>
2026-08-17 17:33:50 +0500

    cleans-up old rest-based sync communication..

--- gym-service/src/main/java/com/gym/GymCrmApplication.java
+++ gym-service/src/main/java/com/gym/GymCrmApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class GymCrmApplication {
     public static void main(String[] args) {
