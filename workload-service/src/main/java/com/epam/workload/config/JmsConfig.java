a3e65105feb4471832dbe99cd707963004fc7e77
aumidov <umidovalimardon06@gmail.com>
2026-08-17 16:12:37 +0500

    adds Obj<->JSON<->Message converter config

---
+++ workload-service/src/main/java/com/epam/workload/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package com.epam.workload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
