ca98cdaa35a4579ee2b66ef9b40acd87f73ce8e3
aumidov <umidovalimardon06@gmail.com>
2026-08-21 11:04:53 +0500

    adds redelivery policy bean

--- workload-service/src/main/java/com/epam/workload/config/JmsConfig.java
+++ workload-service/src/main/java/com/epam/workload/config/JmsConfig.java
@@ -3,6 +3,9 @@ package com.epam.workload.config;
 import java.util.Map;
 import com.epam.workload.dto.WorkloadRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -13,6 +16,23 @@ import org.springframework.jms.support.converter.MappingJackson2MessageConverter
 @EnableJms
 @Configuration
 public class JmsConfig {
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory(
+            @Value("${spring.activemq.broker-url}") String brokerUrl,
+            @Value("${spring.activemq.user}") String user,
+            @Value("${spring.activemq.password}") String password) {
+
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(user, password, brokerUrl);
+
+        RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
+        redeliveryPolicy.setMaximumRedeliveries(3);
+        redeliveryPolicy.setInitialRedeliveryDelay(1000);
+        redeliveryPolicy.setUseExponentialBackOff(false);
+        factory.setRedeliveryPolicy(redeliveryPolicy);
+
+        return factory;
+    }
+
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
