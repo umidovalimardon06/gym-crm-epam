7699877388c68e5e493a04304d9ff71f81dd0ad6
aumidov <umidovalimardon06@gmail.com>
2026-08-21 10:46:08 +0500

    externalize workload queue name @Value -> application-dev properties

--- gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadNotifier.java
+++ gym-service/src/main/java/com/gym/infrastructure/workload/WorkloadNotifier.java
@@ -1,25 +1,26 @@
 package com.gym.infrastructure.workload;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkloadNotifier {
     private static final Logger log = LoggerFactory.getLogger(WorkloadNotifier.class);
-    public static final String QUEUE_NAME = "workload.queue";
-
+    public final String queueName;
     private final JmsTemplate jmsTemplate;
 
-    public WorkloadNotifier(JmsTemplate jmsTemplate) {
+    public WorkloadNotifier(JmsTemplate jmsTemplate,
+                            @Value("${workload.queue.name}") String queueName) {
         this.jmsTemplate = jmsTemplate;
+        this.queueName = queueName;
     }
 
     public void notify(WorkloadRequest request) {
         try {
-            jmsTemplate.convertAndSend(QUEUE_NAME, request);
+            jmsTemplate.convertAndSend(queueName, request);
             log.info("Workload message sent: trainer={}, action={}", request.username(), request.actionType());
         } catch (Exception e) {
             log.warn("Failed to send workload message for trainer={}, action={}: {}",
