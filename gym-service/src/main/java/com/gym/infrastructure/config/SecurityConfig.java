656cee7acfc5d9dd39b28d6eaf37dd86b276c2fa
aumidov <umidovalimardon06@gmail.com>
2026-09-03 09:48:13 +0500

    adds trainer registration feature and steps

--- gym-service/src/main/java/com/gym/infrastructure/config/SecurityConfig.java
+++ gym-service/src/main/java/com/gym/infrastructure/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/trainees").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/trainers").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/trainers").permitAll()
                         .requestMatchers(
                                 HttpMethod.PATCH,
                                 "/api/trainees/activate",
